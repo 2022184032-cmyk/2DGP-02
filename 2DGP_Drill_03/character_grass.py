7cf468d6272d9b5b941241de9835d7c239e11773
2022184032-cmyk <2022184032@tukorea.ac.kr>
2025-09-19 22:53:44 +0900

    파일생성

---
+++ 2DGP_Drill_03/character_grass.py
@@ -0,0 +1,21 @@
+from pico2d import *
+
+open_canvas()
+
+
+def move_rectangle():
+    print("Moving rectangle")
+    pass
+
+
+def move_circle():
+    print("Moving circle")
+    pass
+
+
+while True:
+    move_rectangle()
+    move_circle()
+    pass
+
+close_canvas()
